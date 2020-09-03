b8793cfdd73c7443e344dde37d8f92568d364469
王福军 <wfj@meitu.com>
2020-09-03 08:23:01 +0800

    【feature】增加native player页面入口

--- learnffmpeg/src/main/java/com/meitu/learnffmpeg/MainActivity.kt
+++ learnffmpeg/src/main/java/com/meitu/learnffmpeg/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.meitu.learnffmpeg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.meitu.learnffmpeg.media.FFMediaPlayer
 
@@ -12,5 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val textView = findViewById<TextView>(R.id.textView)
         textView.text = "FFmpeg Version Info:\n" + FFMediaPlayer.getFFmpegVersion()
+
+        val nativePlayer = findViewById<Button>(R.id.nativePlayer)
+        nativePlayer.setOnClickListener {
+            startActivity(Intent(this, NativeMediaPlayerActivity::class.java))
+        }
     }
 }
